2286229e46b443baa75e8ea7a3367befd34ad58b
Suzana Paulino <43155385+SuzanaLima@users.noreply.github.com>
2025-02-26 12:23:30 -0300

    initial commit

---
+++ components/ProfileInfo.vue
@@ -0,0 +1,21 @@
+<template>
+  <figure class="p-6">
+    <img class="w-32 h-32 rounded-full mx-auto" src="../assets/images/profile-pic.jpg" alt="profile picture" width="400"
+      height="400" />
+    <div class="pt-6 text-center space-y-4">
+      <figcaption class="font-medium">
+        <div class="text-yellow-500 text-xl py-2">{{ title }}</div>
+        <div class="text-cyan-900 font-light">{{ subtitle }}</div>
+      </figcaption>
+      <blockquote>
+        <p class="text-lg font-medium py-2">{{ headline }}</p>
+      </blockquote>
+      <hashtag>
+        #Made with 💗   
+      </hashtag>
+    </div>
+  </figure>
+</template>
+<script setup>
+const { title, subtitle, headline } = defineProps(['title', 'subtitle', 'headline'])
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <ProfileInfo title="Suzana Lima" 
+    subtitle="@_svplima"
+    headline="Desenvolvedora Back End Java"/>
+
+  <main class="max-w-xs mx-auto">
+    <ButtonAction link="https://www.linkedin.com/in/suzana-lima/" icon="✍️" text="LinkedIn"/>
+    <ButtonAction link="https://wa.me/+5511949371149" icon="📲" text="Contact"/>
+    <ButtonAction link="https://github.com/SuzanaLima" icon="😺" text="GitHub"/>
+  </main>
+</template>
+
